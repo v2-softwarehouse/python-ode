d07b3d23fba8a009dcaaeae097cfa44537e42903
v2SoftwareHouse <v2.softwarehouse@gmail.com>
2024-05-09 14:58:41 -0300

    remove files

--- ode/value_out_put.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod
-from typing import TypeVar
-from ode.output import Output
-
-V = TypeVar('V')
-
-class ValueOutput(Output[V]):
-    def __init__(self, value: V = None):
-        super().__init__(value, None)
